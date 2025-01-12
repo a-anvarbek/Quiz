b7631aca64d6d45ceb722d3caf444be5fa5f1c75
Anvarbek <c1221sharp@gmail.com>
2025-01-12 11:44:52 +0500

    Html and Css is ready

---
+++ script.js
@@ -0,0 +1,15 @@
+// Quiz
+
+var btn = document.querySelector(".start-quiz button");
+
+btn.addEventListener("click", function() {
+    for(var i = 0; i <= 5; i++){
+        
+    }
+    btn.innerHTML = "Next"
+})
+
+// Question
+function getQuestion(){
+    
+}
